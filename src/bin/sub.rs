b899e28c69dd33527a1ba8577843730fa15d3f8c
Marco Ciotola <848222@stud.unive.it>
2024-10-02 22:39:38 +0200

    feat: more types and traits

--- src/bin/sub.rs
+++ src/bin/sub.rs
@@ -1,10 +1,15 @@
+use samp::utils::signals::Status;
+
 #[tokio::main]
 async fn main() {
     let session = zenoh::open(zenoh::Config::default()).await.unwrap();
     let key = session.declare_keyexpr("windows/*").await.unwrap();
     let subscriber = session.declare_subscriber(key).await.unwrap();
     while let Ok(sample) = subscriber.recv_async().await {
-        println!("Received on {}: {}", sample.key_expr(), sample.payload().deserialize::<String>().unwrap());
+        println!("Received on {}: {}", sample.key_expr(), match sample.payload().deserialize::<Status>() {
+            Ok(x) => x.into(),
+            Err(_) => "<Invalid value>",
+        });
     }
     session.close().await.unwrap();
 }
